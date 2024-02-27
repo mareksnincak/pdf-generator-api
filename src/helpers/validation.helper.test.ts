bb5c1a8fc7dd0be84699cf36a0a8a36e551bf5d5
Marek <m.snincak@gmail.com>
2024-02-27 08:39:11 +0100

    test: cd /Users/personal/dev/pdf-generator-api

--- src/helpers/validation.helper.test.ts
+++ src/helpers/validation.helper.test.ts
@@ -1,10 +1,11 @@
 import z from 'zod';
-import { validateQueryParams } from './validation.helper';
+import { validateBody, validateQueryParams } from './validation.helper';
+import { BadRequestError } from '../errors/bad-request.error';
 
 describe('validateQueryParams', () => {
   it('should pass when data is valid', () => {
     const dto = z.object({
-      foo: z.string().min(1).max(10),
+      foo: z.string(),
     });
 
     const queryStringParameters = { foo: 'bar' };
@@ -13,5 +14,86 @@ describe('validateQueryParams', () => {
     const result = validateQueryParams(event, dto);
 
     expect(result).toEqual(queryStringParameters);
+  });
+
+  it('should transform data when coerce option is used', () => {
+    const dto = z.object({
+      foo: z.coerce.date(),
+    });
+
+    const date = new Date();
+    const queryStringParameters = { foo: date.toISOString() };
+    const event = { queryStringParameters };
+
+    const result = validateQueryParams(event, dto);
+
+    expect(result.foo).toEqual(date);
+  });
+
+  it('should throw error when data is not valid', () => {
+    const dto = z.object({
+      foo: z.number(),
+    });
+
+    const queryStringParameters = { foo: 'bar' };
+    const event = { queryStringParameters };
+
+    try {
+      validateQueryParams(event, dto);
+      expect(true).toEqual(false);
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestError);
+      expect((error as BadRequestError).message).toEqual(
+        'Expected number, received string at "foo"',
+      );
+    }
+  });
+});
+
+describe('validateBody', () => {
+  it('should pass when data is valid', () => {
+    const dto = z.object({
+      foo: z.string(),
+    });
+
+    const body = { foo: 'bar' };
+    const event = { body: JSON.stringify(body) };
+
+    const result = validateBody(event, dto);
+
+    expect(result).toEqual(body);
+  });
+
+  it('should transform data when coerce option is used', () => {
+    const dto = z.object({
+      foo: z.coerce.date(),
+    });
+
+    const date = new Date();
+    const body = { foo: date.toISOString() };
+    const event = { body: JSON.stringify(body) };
+
+    const result = validateBody(event, dto);
+
+    expect(result.foo).toEqual(date);
+  });
+
+  it('should throw error when data is not valid', () => {
+    const dto = z.object({
+      foo: z.number(),
+    });
+
+    const body = { foo: 'bar' };
+    const event = { body: JSON.stringify(body) };
+
+    try {
+      validateBody(event, dto);
+      expect(true).toEqual(false);
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestError);
+      expect((error as BadRequestError).message).toEqual(
+        'Expected number, received string at "foo"',
+      );
+    }
   });
 });
