81a889e808bdd69f0a4f38df385a2830b18114cf
Marek <m.snincak@gmail.com>
2024-03-02 18:14:40 +0100

    test: other endpoints it

--- tests/it/tests/delete-template.it.test.ts
+++ tests/it/tests/delete-template.it.test.ts
@@ -1,3 +1,5 @@
+import { randomUUID } from 'node:crypto';
+
 import { DeleteObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 import { EnvironmentName } from '../../../config/enums/config.enum';
@@ -9,14 +11,14 @@ import { ErrorMessage } from '../../../src/enums/error.enum';
 import { mockLogger } from '../../../src/helpers/test.helper';
 import { deleteTemplate } from '../../../src/lambdas/delete-template/handler';
 import { DeleteTemplateRequestMockFactory } from '../../../src/lambdas/delete-template/mock-factories/request.mock-factory';
-import { ApiGatewayProxyEventMockFactory } from '../../../src/mock-factories/api-gateway-proxy-event.mock-factory';
+import { ApiGatewayProxyWithCognitoAuthorizerEventMockFactory } from '../../../src/mock-factories/api-gateway-proxy-with-cognito-authorizer-event.mock-factory';
 import { ContextMockFactory } from '../../../src/mock-factories/context.mock-factory';
 import { mockAwsCredentials } from '../helpers/credential.helper';
 import { refreshDynamoDb } from '../helpers/dynamo-db.helper';
 
 const requestMockFactory = new DeleteTemplateRequestMockFactory();
-const eventMockFactory = new ApiGatewayProxyEventMockFactory();
-const templateEntity = new TemplateEntityMockFactory().create();
+const eventMockFactory = new ApiGatewayProxyWithCognitoAuthorizerEventMockFactory();
+const templateEntityMockFactory = new TemplateEntityMockFactory();
 const context = new ContextMockFactory().create();
 
 beforeAll(() => {
@@ -36,19 +38,26 @@ describe('deleteTemplate', () => {
   it('should delete template', async () => {
     const s3ClientSpy = jest.spyOn(S3Client.prototype, 'send').mockImplementation();
 
-    await createOrReplace(templateEntity);
-
-    const pathParameters = requestMockFactory.create({ id: templateEntity.id });
+    const templateId = randomUUID();
+    const pathParameters = requestMockFactory.create({ id: templateId });
     const event = eventMockFactory.create({
       pathParameters,
     });
+
+    const userId = event.requestContext.authorizer.claims.sub;
+    const templateEntity = templateEntityMockFactory.create({
+      id: templateId,
+      userId,
+    });
+
+    await createOrReplace(templateEntity);
 
     const result = await deleteTemplate(event, context);
 
     expect(result.statusCode).toEqual(204);
     expect(result.body).toEqual('');
 
-    const templateAfterDeletion = await findById(templateEntity.id);
+    const templateAfterDeletion = await findById({ id: templateEntity.id, userId });
     expect(templateAfterDeletion).toEqual(null);
 
     const s3ClientArgs = s3ClientSpy.mock.calls[0][0];
