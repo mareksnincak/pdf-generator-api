9f36d7f407d38af1abde57836d3de13f5b457241
Marek <m.snincak@gmail.com>
2024-03-03 10:19:22 +0100

    test: create template conflict

--- src/lambdas/create-template/handler.test.ts
+++ src/lambdas/create-template/handler.test.ts
@@ -6,6 +6,7 @@ import { Lambda } from '../../../infra/cdk/enums/lambda.enum';
 import { TemplateEntityMockFactory } from '../../db/template/template.mock-factory';
 import * as templateRepository from '../../db/template/template.repository';
 import { ErrorMessage } from '../../enums/error.enum';
+import { ConflictError } from '../../errors/conflict.error';
 import * as s3Helper from '../../helpers/s3.helper';
 import { mockLogger } from '../../helpers/test.helper';
 import { ApiGatewayProxyWithCognitoAuthorizerEventMockFactory } from '../../mock-factories/api-gateway-proxy-with-cognito-authorizer-event.mock-factory';
@@ -64,5 +65,30 @@ describe('createTemplate', () => {
     expect(JSON.parse(result.body)).toEqual({
       message: ErrorMessage.templateDataNotFound,
     });
+  });
+
+  it('should return 409 and delete s3 data when template already exists', async () => {
+    mockLogger();
+    jest.spyOn(s3Helper, 'moveObject').mockImplementation();
+    const deleteObjectSpy = jest.spyOn(s3Helper, 'deleteObject').mockImplementation();
+    jest.spyOn(templateRepository, 'createOrReplace').mockRejectedValue(
+      new ConflictError({
+        message: ErrorMessage.templateAlreadyExists,
+      }),
+    );
+
+    const requestBody = requestMockFactory.create();
+    const event = eventMockFactory.create({
+      body: JSON.stringify(requestBody),
+    });
+
+    const result = await createTemplate(event, context);
+
+    expect(result.statusCode).toEqual(409);
+    expect(JSON.parse(result.body)).toEqual({
+      message: ErrorMessage.templateAlreadyExists,
+    });
+
+    expect(deleteObjectSpy).toHaveBeenCalled();
   });
 });
