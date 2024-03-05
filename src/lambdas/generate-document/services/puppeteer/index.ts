a62e1a0f3d343b278a52125d312e7c145f42f4cd
Marek <m.snincak@gmail.com>
2024-03-05 10:40:55 +0100

    feat: generate pdf

--- src/lambdas/generate-document/services/puppeteer/index.ts
+++ src/lambdas/generate-document/services/puppeteer/index.ts
@@ -4,8 +4,6 @@ import { launch as launchPuppeteer } from 'puppeteer-core';
 import { logger } from '../../../../helpers/logger.helper';
 
 export async function transformPdfToHtml(html: string) {
-  // Will be implemented in next MR
-
   const browser = await launchPuppeteer({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
@@ -16,11 +14,13 @@ export async function transformPdfToHtml(html: string) {
   logger.debug('puppeteerService.transformPdfToHtml.initialized');
 
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  const pageTitle = await page.title();
+  await page.setContent(html);
+  const pdf = await page.pdf({
+    format: 'A4',
+  });
 
   await browser.close();
 
-  logger.info({ pageTitle }, 'puppeteerService.transformPdfToHtml.success');
-  return await Promise.resolve(Buffer.from(html));
+  logger.info('puppeteerService.transformPdfToHtml.success');
+  return pdf;
 }
