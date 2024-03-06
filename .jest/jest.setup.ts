a684090894f36f8a072b360cbc70e3459df6c194
Marek <m.snincak@gmail.com>
2024-03-06 11:12:14 +0100

    fix: debugging

--- .jest/jest.setup.ts
+++ .jest/jest.setup.ts
@@ -1,17 +1,14 @@
 import { join } from 'path';
 import { EnvironmentName } from '../config/enums/config.enum';
 import { setEnvVarsFromConfig } from '../config/helpers/config.helper';
-import isCi from 'is-ci';
 
 setEnvVarsFromConfig(EnvironmentName.localTest);
 
-if (!isCi) {
-  process.env.CHROMIUM_LOCAL_EXECUTABLE_PATH = join(
-    __dirname,
-    '..',
-    'infra',
-    'local',
-    'chromium',
-    'chrome-headless-shell',
-  );
-}
+process.env.CHROMIUM_LOCAL_EXECUTABLE_PATH = join(
+  __dirname,
+  '..',
+  'infra',
+  'local',
+  'chromium',
+  'chrome-headless-shell',
+);
