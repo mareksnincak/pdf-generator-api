7edb2a149b30fb367cf70aefc4a7626c51d5e4fd
Marek <m.snincak@gmail.com>
2024-03-06 11:26:06 +0100

    fix: debugging

--- src/lambdas/generate-document/services/pdf.service.ts
+++ src/lambdas/generate-document/services/pdf.service.ts
@@ -1,35 +1,40 @@
 import chromium from '@sparticuz/chromium';
-import puppeteer from 'puppeteer-core';
+import puppeteer, { type PuppeteerLaunchOptions } from 'puppeteer-core';
 
 import { logger } from '../../../helpers/logger.helper';
 
 chromium.setHeadlessMode = true;
 chromium.setGraphicsMode = false;
 
-let executablePath: string | undefined;
+let launchOptions: PuppeteerLaunchOptions | undefined;
 
-async function getExecutablePath() {
-  if (executablePath) {
-    return executablePath;
+async function getLaunchOptions() {
+  if (launchOptions) {
+    return launchOptions;
   }
 
-  executablePath = process.env.CHROMIUM_LOCAL_EXECUTABLE_PATH;
-  if (!executablePath) {
-    executablePath = await chromium.executablePath();
+  const defaultArgs = chromium.args;
+  const defaultLaunchOptions: PuppeteerLaunchOptions = {
+    args: defaultArgs,
+    defaultViewport: chromium.defaultViewport,
+    headless: true,
+    executablePath: await chromium.executablePath(),
+  };
+
+  const chromiumLocalExecutablePath = process.env.CHROMIUM_LOCAL_EXECUTABLE_PATH;
+  if (chromiumLocalExecutablePath) {
+    defaultLaunchOptions.executablePath = chromiumLocalExecutablePath;
+    defaultLaunchOptions.args = [...defaultArgs, '--no-sandbox', '--disable-setuid-sandbox'];
   }
 
-  return executablePath;
+  launchOptions = defaultLaunchOptions;
+  return launchOptions;
 }
 
 export async function createPdfFromHtml(html: string) {
   logger.info('generateDocument.pdfService.createPdfFromHtml.start');
 
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await getExecutablePath(),
-    headless: true,
-  });
+  const browser = await puppeteer.launch(await getLaunchOptions());
 
   logger.debug('generateDocument.pdfService.createPdfFromHtml.initialized');
 
