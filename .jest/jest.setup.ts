ffc05f9a04de9972d55b557c64d8d15c284ceaf0
Marek <m.snincak@gmail.com>
2024-03-06 10:35:49 +0100

    test: dont use local chromium in ci

--- .jest/jest.setup.ts
+++ .jest/jest.setup.ts
@@ -1,14 +1,17 @@
 import { join } from 'path';
 import { EnvironmentName } from '../config/enums/config.enum';
 import { setEnvVarsFromConfig } from '../config/helpers/config.helper';
+import isCi from 'is-ci';
 
 setEnvVarsFromConfig(EnvironmentName.localTest);
 
-process.env.CHROMIUM_LOCAL_EXECUTABLE_PATH = join(
-  __dirname,
-  '..',
-  'infra',
-  'local',
-  'chromium',
-  'chrome-headless-shell',
-);
+if (!isCi) {
+  process.env.CHROMIUM_LOCAL_EXECUTABLE_PATH = join(
+    __dirname,
+    '..',
+    'infra',
+    'local',
+    'chromium',
+    'chrome-headless-shell',
+  );
+}
