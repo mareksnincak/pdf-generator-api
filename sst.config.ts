f20f9310761aea4d079cfa25cea85d98f8cd9ff1
Marek Snincak <m.snincak@gmail.com>
2024-11-03 10:14:18 +0100

    chore: provision dynamodb

--- sst.config.ts
+++ sst.config.ts
@@ -1,4 +1,8 @@
 /// <reference path="./.sst/platform/config.d.ts" />
+import { cdkEnvVarsDto } from './infra/cdk/dtos/cdk-env-vars.dto';
+import * as packageJson from './package.json';
+import { DynamoIndex } from './src/db/common/enums/dynamo.enum';
+import { validate } from './src/helpers/validation.helper';
 
 export default $config({
   app(input) {
@@ -9,7 +13,30 @@ export default $config({
     };
   },
   async run() {
-    // const vpc = new sst.aws.Vpc('MyVpc', { bastion: true });
-    // const dynamoDbTable = new sst.aws.Bucket();
+    const cdkEnvVars = validate(process.env, cdkEnvVarsDto);
+    const stackId = `${packageJson.name}-${cdkEnvVars.ENVIRONMENT_NAME}`;
+
+    const dynamoDbTable = new sst.aws.Dynamo(stackId, {
+      fields: {
+        PK: 'string',
+        SK: 'string',
+        GSI1PK: 'string',
+        GSI1SK: 'string',
+      },
+      primaryIndex: { hashKey: 'PK', rangeKey: 'SK' },
+      ttl: 'expiresAt',
+      stream: 'old-image',
+      globalIndexes: {
+        [DynamoIndex.GSI1]: {
+          hashKey: 'GSI1PK',
+          rangeKey: 'GSI1SK',
+          projection: 'all',
+        },
+      },
+    });
+
+    return {
+      tableName: dynamoDbTable.name,
+    };
   },
 });
